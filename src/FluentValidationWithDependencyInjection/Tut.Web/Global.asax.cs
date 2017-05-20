f4621598f4d5afb271dd21f14c961f3420eacb1c
Tran Thuong Thien <tranthiencdsp@gmail.com>
2017-05-20 21:42:52 +0700

    update config file .travis.yml

--- src/FluentValidationWithDependencyInjection/Tut.Web/Global.asax.cs
+++ src/FluentValidationWithDependencyInjection/Tut.Web/Global.asax.cs
@@ -1,18 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using FluentValidation.Mvc;
 
-namespace ValidationUsingFluentValidation.Web
+namespace Tut.Web
 {
-    public class MvcApplication : System.Web.HttpApplication
+    public class MvcApplication : HttpApplication
     {
         protected void Application_Start()
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            FluentValidationModelValidatorProvider.Configure();
         }
     }
 }
